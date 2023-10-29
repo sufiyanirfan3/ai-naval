7ac25059d72c0ac24432e7ef378b21a57649582e
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-10-29 16:09:54 +0500

    batch2

--- scripts/createIndex.ts
+++ scripts/createIndex.ts
@@ -20,7 +20,7 @@ dotenv.config();
   const pineconeIndex = client.Index(process.env.PINECONE_INDEX as string);
 
   // Specify the directory where your PDF files are located
-  const pdfDirectory = "./scripts/batch1"; // Update with your directory path
+  const pdfDirectory = "./scripts/msla"; // Update with your directory path
 
   // Read the files in the directory
   const pdfFiles = fs.readdirSync(pdfDirectory);
@@ -37,7 +37,7 @@ dotenv.config();
 
       const splitter = new CharacterTextSplitter({
         separator: "\n",
-        chunkSize: 2000,
+        chunkSize: 20000,
         chunkOverlap: 200,
       });
       const splitDocs = await splitter.splitDocuments(docs);
