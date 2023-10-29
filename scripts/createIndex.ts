48ca9ae510b3640c4ad9b1960a7220e4e96be022
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-10-29 17:28:51 +0500

    batch4 done

--- scripts/createIndex.ts
+++ scripts/createIndex.ts
@@ -20,7 +20,7 @@ dotenv.config();
   const pineconeIndex = client.Index(process.env.PINECONE_INDEX as string);
 
   // Specify the directory where your PDF files are located
-  const pdfDirectory = "./scripts/batch3"; // Update with your directory path
+  const pdfDirectory = "./scripts/batch4"; // Update with your directory path
 
   // Read the files in the directory
   const pdfFiles = fs.readdirSync(pdfDirectory);
