2bed173bc8c926a3c847344ef2dc0cd18d6a787a
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-11-01 16:12:27 +0500

    testing

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -31,7 +31,7 @@ async function askOpenAI({
   const pinecone = await pineconeStore();
 
   console.log("messages req: ", messages);
-
+  var updatedMsgContent;
   // updated the message content to include context snippets
   if (messages?.length > 0) {
     const lastMsgContent = messages[messages.length - 1].content;
@@ -40,17 +40,17 @@ async function askOpenAI({
 
     console.log("pinecone data.length: ", data.length);
 
-    // const updatedMsgContent=`${lastMsgContent}`
-    const updatedMsgContent = `
-    user question/statement: ${lastMsgContent}
-    context snippets:
-    ---
-    1) ${data?.[0]?.pageContent}
-    ---
-    2) ${data?.[1]?.pageContent}
-    ---
-    3) ${data?.[2]?.pageContent}
-    `;
+    updatedMsgContent = `${lastMsgContent}`;
+    // const updatedMsgContent = `
+    // user question/statement: ${lastMsgContent}
+    // context snippets:
+    // ---
+    // 1) ${data?.[0]?.pageContent}
+    // ---
+    // 2) ${data?.[1]?.pageContent}
+    // ---
+    // 3) ${data?.[2]?.pageContent}
+    // `;
 
     messages[messages.length - 1].content = updatedMsgContent;
   }
@@ -66,12 +66,16 @@ async function askOpenAI({
         Introduce youself to ${userName}. Don't mention context snippets when replying to user and only mention yourself by your first name. Answer very shortly. Answer only according to current context. Dont take into account previous messages they are just for reference. Answer 
         `,
         },
-        ...(messages || [
-          {
-            role: "user",
-            content: "Hi There!",
-          },
-        ]),
+        {
+          role: "user",
+          content: `${updatedMsgContent}`,
+        },
+        //   ...(messages || [
+        //     {
+        //       role: "user",
+        //       content: "Hi There!",
+        //     },
+        //   ]),
       ],
     });
 
